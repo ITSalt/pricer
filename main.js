8b4e802dc094c15193d7de61c6468922ff7801df
Max Nikitin <magznikitin@gmail.com>
2021-12-25 02:04:37 +0300

    Add button for add to desktop

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ const changeMe = (e) => {
 
   let compareText = "";
   if (sums.money && sums.money2)
-    compareText = sums.money < sums.money2 ? "<<< Первый выгоднее!" : (sums.money > sums.money2 ? "Второй выгоднее! >>>>" : "<<< Однофигово >>>");
+    compareText = sums.money < sums.money2 ? "<<< Первый выгоднее!" : (sums.money > sums.money2 ? "Второй выгоднее! >>>" : "<<< Однофигово >>>");
   
   $$(`#txtChecks${addToName}`).innerHTML = minds.join("/r/n");
   $$("#resume").innerHTML = compareText;
@@ -32,15 +32,50 @@ const changeMe = (e) => {
   
 }
 
-const changeMeToo = (e) => {
-
-}
-
 const afterLoad = () => {
   $$('#txtMoney').addEventListener("keyup", changeMe);
   $$('#txtValue').addEventListener("keyup", changeMe);
   $$('#txtMoney2').addEventListener("keyup", changeMe);
   $$('#txtValue2').addEventListener("keyup", changeMe);
+
+  // Register service worker to control making site work offline
+
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+      .register('sw.js')
+      .then(() => { console.log('Service Worker Registered'); });
+  }
+
+  // Code to handle install prompt on desktop
+
+  let deferredPrompt;
+  const addBtn = document.querySelector('.add-button');
+  addBtn.style.display = 'none';
+
+  window.addEventListener('beforeinstallprompt', (e) => {
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e;
+    // Update UI to notify the user they can add to home screen
+    addBtn.style.display = 'block';
+
+    addBtn.addEventListener('click', () => {
+      // hide our user interface that shows our A2HS button
+      addBtn.style.display = 'none';
+      // Show the prompt
+      deferredPrompt.prompt();
+      // Wait for the user to respond to the prompt
+      deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the A2HS prompt');
+        } else {
+          console.log('User dismissed the A2HS prompt');
+        }
+        deferredPrompt = null;
+      });
+    });
+  });
 }
 
 document.addEventListener('DOMContentLoaded', afterLoad);
