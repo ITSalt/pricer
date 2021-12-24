59ad779da6fbf0342bf9be2394367f7fd49e5d1e
Max Nikitin <magznikitin@gmail.com>
2021-12-25 02:21:35 +0300

    Remove pieces

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ const changeMe = (e) => {
 
   if (val.price && val.value) {
     sums[`money${addToName}`] = +(val.price / val.value * 1000).toFixed(2);
-    minds.push(`За кг\\л\\1000шт - <span class="big">${sums[`money${addToName}`].toLocaleString()}</span> денег`);
+    minds.push(`За кг\\л - <span class="big">${sums[`money${addToName}`].toLocaleString()}</span> денег`);
   }
 
   let compareText = "";
