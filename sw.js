7af9b8f87370f0a950b22a965e58a923dee27262
Max Nikitin <magznikitin@gmail.com>
2021-12-28 13:06:08 +0300

    Attepmt 2 fixing A2HS

--- sw.js
+++ sw.js
@@ -1,4 +1,36 @@
-self.addEventListener('install', (e) => {
+self.addEventListener('install', function (event) {
+  console.log('[ServiceWorker] Install');
+
+  event.waitUntil((async () => {
+    const cache = await caches.open("pricerStore");
+    // Setting {cache: 'reload'} in the new request will ensure that the response
+    // isn't fulfilled from the HTTP cache; i.e., it will be from the network.
+    await cache.addAll([
+      new Request("index.html", { cache: 'reload' }),
+      new Request("main.js", { cache: 'reload' }),
+      new Request("main.css", { cache: 'reload' })
+    ]);
+  })());
+
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('[ServiceWorker] Activate');
+  event.waitUntil((async () => {
+    // Enable navigation preload if it's supported.
+    // See https://developers.google.com/web/updates/2017/02/navigation-preload
+    if ('navigationPreload' in self.registration) {
+      await self.registration.navigationPreload.enable();
+    }
+  })());
+
+  // Tell the active service worker to take control of the page immediately.
+  self.clients.claim();
+});
+
+
+/*self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open('fox-store').then((cache) => cache.addAll([
       '/',
@@ -7,7 +39,7 @@ self.addEventListener('install', (e) => {
       '/main.css'
     ])),
   );
-});
+});*/
 
 self.addEventListener('fetch', (e) => {
   console.log("fetch", e.request.url);
